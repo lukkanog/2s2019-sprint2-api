30c9d3f06ffe8a9a9b061e97a2c3a3e4189a7916
Lucas Nogueira De Souza <46735197879@info.local>
2019-08-20 11:25:40 -0300

    :hatching_chick:

---
+++ Senai.Filmes.WebApi/Senai.Filmes.WebApi/Controllers/GenerosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Filmes.WebApi.Domains;
+using Senai.Filmes.WebApi.Repositories;
+
+namespace Senai.Filmes.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        GeneroRepository generoRepository = new GeneroRepository();
+
+        [HttpGet]
+        public IEnumerable<GeneroDomain> Listar()
+        {
+            return generoRepository.Listar();
+            
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(GeneroDomain genero)
+        {
+            generoRepository.Cadastrar(genero);
+            return Ok();
+        }
+
+        //[HttpPut("{id}")]
+        //public IActionResult Alterar(int id)
+        //{
+        //    generoRepository.Alterar(id);
+        //    return Ok();
+        //}
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            generoRepository.Deletar(id);
+            return Ok();
+        }
+    }
+}
---
+++ Senai.Filmes.WebApi/Senai.Filmes.WebApi/Repositories/GeneroRepository.cs
@@ -0,0 +1,86 @@
+﻿using Senai.Filmes.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Filmes.WebApi.Repositories
+{
+    public class GeneroRepository
+    {
+        private string StringConexao =
+            "Data Source=.\\SqlExpress; initial catalog=RoteiroFilmes; User Id=sa;Pwd=132";
+
+        public List<GeneroDomain> Listar()
+        {
+            List<GeneroDomain> generos = new List<GeneroDomain>();
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string Query = "SELECT * FROM Generos ORDER BY IdGenero ASC";
+
+                con.Open();
+                SqlDataReader srd;
+                using (SqlCommand cmd = new SqlCommand(Query, con))
+                {
+                    srd = cmd.ExecuteReader();
+                    
+                    while (srd.Read())
+                    {
+                        GeneroDomain estilo = new GeneroDomain
+                        {
+                            Id = Convert.ToInt32(srd["IdGenero"]),
+                            Nome = srd["Nome"].ToString()
+                        };
+                        generos.Add(estilo);
+                    }
+                }
+            }
+            return generos;
+        }//                fim do metodo               // 
+
+
+        public void Cadastrar(GeneroDomain genero)
+        {
+            string Query = "INSERT INTO Generos (Nome) VALUES (@Nome)";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@Nome", genero.Nome);
+                cmd.ExecuteNonQuery();
+            }
+        }//                fim do metodo               //
+
+
+        //public void Alterar(int id)
+        //{
+        //    string Query = "UPDATE Generos SET Nome = 'teste' WHERE IdGenero = @IdGenero";
+
+        //    using (SqlConnection con = new SqlConnection(StringConexao))
+        //    {
+        //        con.Open();
+        //        SqlCommand cmd = new SqlCommand(Query, con);
+        //        cmd.Parameters.AddWithValue("@IdGenero",id);
+        //        ////cmd.Parameters.AddWithValue("@Nome", );
+        //    }
+        //}
+
+
+        public void Deletar(int id)
+        {
+            string Query = "DELETE Generos WHERE IdGenero = @IdGenero";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@IdGenero",id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
